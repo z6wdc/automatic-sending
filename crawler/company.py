1ebeac911964e32fe34709ec56a4fc5e136e31ac
z6wdc <whitedeathcross@gmail.com>
2021-09-21 21:32:45 +0900

    company list csv file

--- crawler/company.py
+++ crawler/company.py
@@ -1,7 +1,13 @@
 """
 The crawler which collect the company name and URL
 
+source: indeed
+source: green
+
+https://jp.indeed.com/%E6%B1%82%E4%BA%BA?q=%E3%83%87%E3%83%BC%E3%82%BF%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%83%86%E3%82%A3%E3%82%B9%E3%83%88&limit=15&start=0
+
 Expected format:
 企業名,URL
 Humanevalue株式会社,http://hmev.co.jp
 """
+
