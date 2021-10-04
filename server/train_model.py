b5ead752c309031baf3144e5096b005ace35af57
劉家妏 <yuka.cw.liu@gmail.com>
2021-10-04 23:09:07 +0800

    lsi model

--- server/train_model.py
+++ server/train_model.py
@@ -1,18 +1,31 @@
 from model.fnn_simple import FNNSimpleEstimator
+from model.lsi import LsiEstimator
 from adapter.training import CSVTrainingAdapter
 
 
 def main():
-    model = FNNSimpleEstimator()
-    options = {
-        'ratio_test': 0.2,
-        'seed': 100,
-    }
+    model = 'lsi'
+    if model == 'fnn':
+        model = FNNSimpleEstimator()
+        options = {
+            'ratio_test': 0.2,
+            'seed': 100,
+        }
 
-    adapter = CSVTrainingAdapter(options)
-    model.set_adapter(adapter)
-    model.train({'epochs': 400, 'verbose': 0})
-    model.save("./models/fnn_simple")
+        adapter = CSVTrainingAdapter(options)
+        model.set_adapter(adapter)
+        model.train({'epochs': 400, 'verbose': 0})
+        model.save("./models/fnn_simple")
+    elif model == 'lsi':
+        model = LsiEstimator()
+        options = {
+            'ratio_test': 0.2,
+            'seed': 100,
+        }
+        adapter = CSVTrainingAdapter(options)
+        model.set_adapter(adapter)
+        model.train()
+        model.save("./models/lsi")
 
 
 if __name__ == "__main__":
